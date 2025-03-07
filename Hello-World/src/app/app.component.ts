08234952fc53938a02fd02463ebc49c92d6644f0
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-03-07 15:18:44 +0530

    [Anush] Add. display BridgeLabz logo using property binding in AppComponent

--- Hello-World/src/app/app.component.ts
+++ Hello-World/src/app/app.component.ts
@@ -3,14 +3,16 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone:true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Hello-World';
+  imgUrl="../../assets/BL_logo_square_jpg.jpg"
 
   ngOnInit(): void{
-    this.title = "Hello from BridgeLabz.";
+    this.title = "Hello from BridgeLabz";
   }
 }
