07ef7de36ec90f8fff021ef630ac75a8f8d60b17
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-03-07 16:13:35 +0530

    [Anush] Add. Validation in user input

--- Hello-World/src/app/app.component.ts
+++ Hello-World/src/app/app.component.ts
@@ -1,26 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone:true,
+  standalone: true,
   imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Hello-World';
-  imgUrl="../../assets/BL_logo_square_jpg.jpg";
+  imgUrl = "../../assets/BL_logo_square_jpg.jpg";
   url: string = "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string = "";
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.title = "Hello from BridgeLabz";
   }
 
-  onClick($event:Event){
+  onClick($event: Event) {
     console.log("save button is clicked!", $event);
-    window.open(this.url,"_blank");
+    window.open(this.url, "_blank");
+  }
+
+  onInput(){
+    console.log("Change Event Occured!");
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect!";
+    
   }
 }
