97c460af0ff329806f744ccfbf507b3e7463e74c
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-03-07 15:31:07 +0530

    [Anush] Add. adding link to logo

--- Hello-World/src/app/app.component.ts
+++ Hello-World/src/app/app.component.ts
@@ -10,9 +10,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Hello-World';
-  imgUrl="../../assets/BL_logo_square_jpg.jpg"
+  imgUrl="../../assets/BL_logo_square_jpg.jpg";
+  url: string = "https://www.bridgelabz.com";
 
   ngOnInit(): void{
     this.title = "Hello from BridgeLabz";
+  }
+
+  onClick($event:Event){
+    console.log("save button is clicked!", $event);
+    window.open(this.url,"_blank");
   }
 }
